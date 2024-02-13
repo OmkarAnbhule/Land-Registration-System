d3c37a42465fffb221dac3436a2701564201727f
OmkarAnbhule <145119971+OmkarAnbhule@users.noreply.github.com>
2024-02-13 15:44:51 +0000

    update

--- agriculture/src/components/Navbar.jsx
+++ agriculture/src/components/Navbar.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 
 export default function Navbar() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='navbar-root'>
+            <div className='logo'>
+                <img width={50} height={50}></img>
+                <p>Some Name</p>
+            </div>
+            <div className='icon-control'>
+
+            </div>
+            <div className='sidebar'>
+                <div className='logo'>
+                    <img width={50} height={50}></img>
+                    <p>Some Name</p>
+                </div>
+                <p>Home</p>
+                <p>About</p>
+                <p>Contact</p>
+            </div>
+            <div className='links'>
+                <p>Home</p>
+                <p>About</p>
+                <p>Contact</p>
+            </div>
+            <div className='profile'>
+                <img></img>
+                <button>Log in</button>
+                <button>Sign Up</button>
+            </div>
+        </div>
+    )
 }
 
