5134ce36fee1332f9d1e343b421a8a1304c015a4
OmkarAnbhule <145119971+OmkarAnbhule@users.noreply.github.com>
2024-02-13 14:31:06 +0000

    update

--- agriculture/src/App.jsx
+++ agriculture/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import Register from './components/forms/Register/Register'
 import Home from './components/Home'
+import Navbar from './components/Navbar'
 function App() {
   return (
     <>
+    <Navbar></Navbar>
     <Home></Home>
     </>
   )
