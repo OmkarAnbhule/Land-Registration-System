7ac97a641fd0dec21f606a2648ca38c77c856801
OmkarAnbhule <145119971+OmkarAnbhule@users.noreply.github.com>
2024-02-15 15:32:06 +0000

    update

--- agriculture/src/components/Home.jsx
+++ agriculture/src/components/Home.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      helo
+    <div className='home-root'>
+      <div className='text'>
+        <p>
+          Land Registration<br/> System
+        </p>
+        <p>
+          lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
+        </p>
+
+      </div>
+      <div className='image'>
+        <img width={500} height={500}></img>
+      </div>
+      <div className='background'>
+        <div className='shape'>
+          
+        </div>
+      </div>
     </div>
   )
 }
