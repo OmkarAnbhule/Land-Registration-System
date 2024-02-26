438d3ea2ea5e4741c1a1245a4a42e4ef993b2c22
awsuser1223 <omkaranbhule1223@gmail.com>
2024-02-26 21:59:24 +0530

    update backend

--- agriculture/backend/index.cjs
+++ agriculture/backend/index.cjs
@@ -1,2 +1,18 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/Sociolink')
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/LandLedger');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const cors = require("cors");
+const multer = require('multer')
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, resp) => {
+    resp.send("App is Working");
+});
+
+
+
+
+app.listen(5000)
